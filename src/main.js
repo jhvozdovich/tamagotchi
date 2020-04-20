99b026bff895c749630c041515497619f43ce5f5
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-20 11:14:44 -0700

    add import statements for new classes

--- src/main.js
+++ src/main.js
@@ -2,3 +2,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 // import $ from "jquery";
+
+// import { Tamo } from '/.tamagotchi';
+
+// import { Player } from '/.tamogotchi';
