8692624fb3f97235470c3dbdec50ea602fd9193b
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-21 14:07:35 -0700

    add html and jquery logic to incorporate background images given weather api data

--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-// import $ from "jquery";
+import $ from "jquery";
+// import { Tama } from './tamagotchi';
+// import { Player } from './tamagotchi';
 
-// import { Tama } from '/.tamagotchi';
+$(document).ready(function() {
+  $("#locationButton").click(function() {
+    const city = $("#location").val();
+    $('location').val("");
 
-// import { Player } from '/.tamagotchi;
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY_WEATHER}`;
+
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    const getElements = function(response) {
+      console.log(response.weather[0].main);
+      if (response.weather[0].main === "Rain" || response.weather[0].main === "Drizzle" || response.weather.main === "Thunderstorm") {
+        $('.weather').text(`It is a rainy day!`);
+      } else if (response.weather[0].main === "Clouds") {
+        $('.weather').text(`It is a cloudy day!`);
+      } else {
+        $('.weather').text(`It is a clear day!`);
+      }
+    };
+  });
+});
